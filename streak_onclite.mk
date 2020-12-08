0ec4aae83042f04cd0ea9f3171a4adf5975e4016
MumetNgoding <bryanhafidz@hack.id>
2020-12-08 03:13:02 +0000

    Onclite: Update Fingerprint Desc 2020  google/coral/coral:11/RQ1A.201205.008/6943376:user/release-keys build.id=RP1A.201105.002

--- streak_onclite.mk
+++ streak_onclite.mk
@@ -31,12 +31,12 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.201205.008/6943376:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RD1A.201105.003.C1 6886399 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RQ1A.201205.008 6943376 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys
+    ro.build.fingerprint=google/coral/coral:11/RQ1A.201205.008/6943376:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
